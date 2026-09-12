67a80fbfa44611af2460be4fb2c9f8b5d01e0171
NileKnight0 <ammartube777@gmail.com>
2026-09-12 15:05:57 +0300

    chnged lighting mechanics

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,14 +3,63 @@ extends Node2D
 func init_game():
 	init_lights()
 func init_lights():
-	$map/black.visible = 1
-	$player/flash.visible = 1
+	#$map/black.visible = 1
+	#$player/flash.visible = 1
+	
+	$map/hallway/dark.visible = 1
+	$map/part1/dark.visible = 1
+	$map/part2/dark.visible = 1
+	$map/part3/dark.visible = 1
+	$map/part4/dark.visible = 1
+	
+	
 
 func _ready() -> void:
-	#dinit_game()
-	
+	init_game()
 	
 	pass
 
 func _process(delta: float) -> void:
 	pass
+
+func _on_area_part1_body_entered(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part1/dark, "modulate:a", 0.0, 0.5)
+func _on_area_part1_body_exited(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part1/dark, "modulate:a", 1.0, 0.5)
+func _on_area_part2_body_entered(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part2/dark, "modulate:a", 0.0, 0.5)
+func _on_area_part2_body_exited(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part2/dark, "modulate:a", 1.0, 0.5)
+func _on_area_hallway_body_entered(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/hallway/dark, "modulate:a", 0.0, 0.5)
+func _on_area_hallway_body_exited(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/hallway/dark, "modulate:a", 1.0, 0.5)
+func _on_area_part3_body_entered(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part3/dark, "modulate:a", 0.0, 0.5)
+func _on_area_part3_body_exited(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part3/dark, "modulate:a", 1.0, 0.5)
+func _on_area_part4_body_entered(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part4/dark, "modulate:a", 0.0, 0.5)
+func _on_area_part4_body_exited(body: Node2D) -> void:
+	if body == $player:
+		var tween = create_tween()
+		tween.tween_property($map/part4/dark, "modulate:a", 1.0, 0.5)
+		
